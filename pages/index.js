e81c6a1d11c7d1dc9bfdb68565885e2db723cb83
samsisle <samko9522@gmail.com>
2020-09-06 16:16:16 -0700

    Update Index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-export default function index(props) {
+import Link from 'next/link';
+
+export default function Index(props) {
   return (
-    <>
+    <div>
       <div className='title'>{props.hygge}</div>
       <div className='author'>By {process.env.NEXT_PUBLIC_AUTHOR}</div>
-    </>
+      <Link href="/uuid">Uuid</Link>
+    </div>
   );
 }
 
