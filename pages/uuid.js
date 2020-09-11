14d480abe3872d1e38ee09e90a3728050b63478b
samsisle <samko9522@gmail.com>
2020-09-11 16:16:23 -0700

    Add uuid page

---
+++ pages/uuid.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
+
+export default function Guid({ uuid }) {
+  return (
+    <div>
+      <div className='uuid'>ID: {uuid}</div>
+      <Link href='/'>Home</Link>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const uuid = uuidv4();
+
+  return {
+    props: {
+      uuid,
+    },
+    revalidate: 10,
+  };
+}
