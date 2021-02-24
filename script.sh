be318dd9de238bd6675bab002e1b48d049e3ac98
samsisle <samko9522@gmail.com>
2021-02-24 13:19:31 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_ENV" == "production" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
