56ca2283290705942e7920cde55fce469903fa83
samsisle <samko9522@gmail.com>
2021-04-17 22:38:14 -0700

    Fix no-anonymous-default-export

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,8 +1,10 @@
-export default (req, res) => {
-  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  if (ip.substr(0, 7) == "::ffff:") {
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  if (ip.substr(0, 7) == '::ffff:') {
     ip = ip.substr(7);
   }
 
   res.status(200).json({ ip });
-};
+}
