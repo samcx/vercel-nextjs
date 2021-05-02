a218ba43d762e3eeb649ae844ca1f1cfad85ff4d
samsisle <samko9522@gmail.com>
2021-05-02 16:59:46 -0700

    Add redirects

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/test/:match*',
+        destination: 'https://vercel.com/:match*',
+        permanent: true,
+      },
+    ];
+  },
 };
