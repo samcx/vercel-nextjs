6c091df04d902059ba737dc295d18bf5d199ef20
samsisle <samko9522@gmail.com>
2021-05-01 17:32:17 -0700

    Update api route function names

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default function ip(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   if (ip.substr(0, 7) == '::ffff:') {
     ip = ip.substr(7);
--- pages/api/time.js
+++ pages/api/time.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default function time(req, res) {
   res.status(200).json({ time: new Date().toLocaleString() });
 }
