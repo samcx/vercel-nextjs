e0d361c9cabd866ed818e289222cc3583ce737f8
samsisle <samko9522@gmail.com>
2021-05-04 01:28:06 -0700

    Update Test page

--- pages/test.js
+++ pages/test.js
@@ -2,8 +2,8 @@ export default function test({ test }) {
   return <div>{test}</div>;
 }
 
-export async function getServerSideProps() {
-  console.log('testing Hello World!');
+export async function getServerSideProps(context) {
+  console.log('req.headers.host =', context.req.headers.host)
   return {
     props: {
       test: "Hello World!",
