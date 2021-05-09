20d663e1dfccec83b07e2721c515cbf1a3daebcb
samsisle <samko9522@gmail.com>
2021-05-09 12:36:42 -0700

    Update pages/api/sub

--- pages/api/sub.js
+++ pages/api/sub.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fetch from 'node-fetch'
 
 export default async function sub(req, res) {
   const sub = await fetch('https://sub-api.dono.gg/v2/subscriptions/active');
