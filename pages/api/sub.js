a7ca500ba6877932b65dd9196e78a2f079cd10c5
samsisle <samko9522@gmail.com>
2021-05-09 12:44:27 -0700

    Update pages/api/sub

--- pages/api/sub.js
+++ pages/api/sub.js
@@ -5,8 +5,7 @@ export default async function sub(req, res) {
   const sub = await fetch('https://sub-api.dono.gg/v2/subscriptions/active', {
     method: 'GET',
     headers: {
-      'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+      'Content-Type': 'application/json',
     },
   });
   const subData = await sub.json();
