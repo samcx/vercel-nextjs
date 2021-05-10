8afc7e4e90e4e5033e9c942a6130f0d1c8be0fe1
samsisle <samko9522@gmail.com>
2021-05-10 12:41:45 -0700

    Update pages/api/sub

--- pages/api/sub.js
+++ pages/api/sub.js
@@ -1,8 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 export default async function sub(req, res) {
-  const sub = await fetch('https://sub-api.dono.gg/v2/subscriptions/active');
+  const sub = await fetch('https://sub-api.dono.gg/v2/subscriptions/active', {
+    method: 'GET',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+    },
+  });
   const subData = await sub.json();
 
   res.json({ data: subData });
