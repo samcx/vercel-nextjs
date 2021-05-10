27a688e3e637204ad7f666bc5fb5d4411fdcd0af
samsisle <samko9522@gmail.com>
2021-05-10 12:54:24 -0700

    Update pages/api/sub

--- pages/api/sub.js
+++ pages/api/sub.js
@@ -4,11 +4,9 @@ import fetch from 'node-fetch';
 export default async function sub(req, res) {
   const sub = await fetch('https://sub-api.dono.gg/v2/subscriptions/active', {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
   });
+
   const subData = await sub.json();
 
-  res.json({ data: subData });
+  res.status(200).json(JSON.stringify(subData));
 }
