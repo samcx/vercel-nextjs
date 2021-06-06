c8c14a92906ae2a88add397606ebeed6da724905
samsisle <samko9522@gmail.com>
2021-06-06 01:04:27 -0700

    Update pages/api/sub

--- pages/api/sub.js
+++ pages/api/sub.js
@@ -1,22 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 export default async function sub(req, res) {
   try {
-    const response = await fetch(
-      'https://sub-api.dono.gg/v2/subscriptions/active',
-      {
-        method: 'GET',
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
-        },
-      }
+    const donoResponse = await axios.get(
+      'https://sub-api.dono.gg/v2/subscriptions/active'
     );
 
-    const data = await response.json();
+    const responseObject = {
+      status: 'success',
+      tiers: [
+        donoResponse.data.data[0].price,
+        donoResponse.data.data[1].price,
+        donoResponse.data.data[2].price,
+      ],
+      message: '',
+    };
 
-    res.json(JSON.stringify(data));
+    return res.json(responseObject);
   } catch (err) {
     return res.json({
       status: 'failed',
