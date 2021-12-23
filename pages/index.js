f2a0fa3c02ae679607950f5b174239912eaf12c8
samsisle <samko9522@gmail.com>
2021-12-22 16:46:49 -0800

    test system environment variable

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ export default function Index(props) {
     <div>
       <div className='title'>{props.hygge}</div>
       <div className='author'>By {process.env.NEXT_PUBLIC_AUTHOR}</div>
+      <div className='author'>
+        NEXT_PUBLIC_VERCEL_ENV = {process.env.NEXT_PUBLIC_VERCEL_ENV}
+      </div>
       {/* <Link href='/uuid'>Uuid</Link> */}
     </div>
   );
