7e1bbe4c8ee62dbf90286587d1e037a523bf3394
samsisle <samko9522@gmail.com>
2022-01-26 17:37:46 -0800

    Update pages/api/ip.js

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function ip(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   if (ip.substr(0, 7) == '::ffff:') {
     ip = ip.substr(7);
   }
