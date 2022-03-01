d3a4731a5315c5be6526c364c8deb6b769528c2e
samsisle <samko9522@gmail.com>
2022-02-28 16:38:14 -0800

    Update rewrites

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 // next.config.js
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/bruv/:slug*',
-        destination: `/:slug*`, // how do I lowercase everything for :slug*
-        permanent: false,
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
         source: '/yeet',
-        destination: 'https://samsisle.com/',
+        destination: 'https://test.com/',
       },
     ];
   },
