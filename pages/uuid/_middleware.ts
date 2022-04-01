7e8df6d2bb2af023c35bce6f34011f467e44892e
samsisle <samko9522@gmail.com>
2022-04-01 14:11:17 -0700

    Add console.log to middleware

--- pages/uuid/_middleware.ts
+++ pages/uuid/_middleware.ts
@@ -3,5 +3,6 @@
 import type { NextFetchEvent, NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  console.log('testing logging!')
   return new Response('Hello, world!')
 }
