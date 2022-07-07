fd7165b34e201556634a9e86474a270ce4c1395c
samsisle <samko9522@gmail.com>
2022-07-06 23:57:20 -0700

    Remove old middleware stuff

--- pages/uuid/_middleware.ts
+++
@@ -1,8 +0,0 @@
-// pages/_middleware.ts
-
-import type { NextFetchEvent, NextRequest } from 'next/server'
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log('testing logging!')
-  return new Response('Hello, world!')
-}
