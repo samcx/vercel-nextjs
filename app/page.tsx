3e916ae016d533fe789beeff78cf9063f22b5172
Sam Ko <sam@vercel.com>
2023-01-22 16:35:45 -0800

    Add image to Index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import img from '../public/327321892_951653379152139_5900064786812746307_n.jpg'
+
 export default function Page() {
   return (
     <div>
@@ -5,6 +8,7 @@ export default function Page() {
       <div className="author">
         NEXT_PUBLIC_VERCEL_ENV = {process.env.NEXT_PUBLIC_VERCEL_ENV}
       </div>
+      <Image alt="bracelet" height={500} src={img} width={300} />
     </div>
   )
 }
