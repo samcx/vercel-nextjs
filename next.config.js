3e95d6992f603d69824799d14d15bff17ca7380d
Sam Ko <sam@vercel.com>
2023-06-13 21:58:21 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
