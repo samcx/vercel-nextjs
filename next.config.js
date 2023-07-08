bc67873217472be95d94abe4b231483da0c6b5f5
Sam Ko <sam@vercel.com>
2023-07-08 00:19:24 -0700

    Upgrade next to 13.4.9

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/bruh',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
