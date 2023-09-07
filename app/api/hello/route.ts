3a396b1c199bc214c80c07634bfe61e375d9565c
Sam Ko <sam@vercel.com>
2023-09-07 10:42:16 -0400

    Check if process.env.VERCEL exists

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  return NextResponse.json({ hello: 'Next.js' })
+  return NextResponse.json({ hello: `${process.env.VERCEL}` })
 }
