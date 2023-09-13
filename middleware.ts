09cd4ebc5480ca09e89c3aa76ebeba73f529c8a7
Sam Ko <sam@vercel.com>
2023-09-13 12:17:57 -0700

    Test error handling in Middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  try {
+    throw new TypeError("oops");
+  } catch (err) {
+    return NextResponse.json({ bruh: `${err}` })
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
