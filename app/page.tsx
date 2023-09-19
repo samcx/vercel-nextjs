88e5ee97ebc454a603a644e13b0d5982030f2461
Sam Ko <sam@vercel.com>
2023-09-18 17:55:38 -0700

    Test trailing white space

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,12 @@ export default function Page() {
       <div className="author">
         NEXT_PUBLIC_VERCEL_ENV = {process.env.NEXT_PUBLIC_VERCEL_ENV}
       </div>
+      <Image
+        src="https://chiru-transparent.s3.us-west-1.amazonaws.com/beanz/17720.png "
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'chiru-transparent.s3.us-west-1.amazonaws.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
