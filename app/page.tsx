7c7c7c04f54f83f220bb071b90091dd681e7ff41
Sam Ko <sam@vercel.com>
2023-09-20 21:22:37 -0700

    Test .env file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Inter } from 'next/font/google'
-import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,12 +9,6 @@ export default function Page() {
       <div className="author">
         NEXT_PUBLIC_VERCEL_ENV = {process.env.NEXT_PUBLIC_VERCEL_ENV}
       </div>
-      <Image
-        src="https://chiru-transparent.s3.us-west-1.amazonaws.com/beanz/17720.png "
-        alt="Picture of the author"
-        width={500}
-        height={500}
-      />
     </div>
   )
 }
