8beaf95fd4643594690a6365cb6f40209fc20355
Sam Ko <sam@vercel.com>
2023-11-04 17:15:59 -0700

    Update index page

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ export default function Page() {
   return (
     <div className={inter.className}>
       <div className="author">By {process.env.NEXT_PUBLIC_AUTHOR}</div>
-      <div className="author">
-        NEXT_PUBLIC_VERCEL_ENV = {process.env.NEXT_PUBLIC_VERCEL_ENV}
-      </div>
     </div>
   )
 }
