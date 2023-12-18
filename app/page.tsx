313c3f66065a1c213c8902d3831914f72f97be0b
Sam Ko <sam@vercel.com>
2023-12-17 23:10:59 -0800

    Test husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Page() {
   return (
     <div className={inter.className}>
-      <div className="author">Byy {process.env.NEXT_PUBLIC_AUTHOR}</div>
+      <div className="author">By {process.env.NEXT_PUBLIC_AUTHOR}</div>
     </div>
-  )
+  );
 }
