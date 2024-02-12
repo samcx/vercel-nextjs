35f60f0f122bc49959b2c049c5550866fcdef987
samcx <sam@vercel.com>
2024-02-12 09:13:12 -0800

    Format correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google'
 
-import "./global.css";
+import './global.css'
 
 const inter = Inter({
-  display: "swap",
-  subsets: ["latin"],
-});
+  display: 'swap',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Vercel → Next.js",
-  description: "Welcome to Next.js",
-};
+  title: 'Vercel → Next.js',
+  description: 'Welcome to Next.js',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
-  );
+  )
 }
