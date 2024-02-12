e776dd1ba919076a7cee20076d081f235be1c961
samcx <sam@vercel.com>
2024-02-12 09:01:29 -0800

    Upgrade packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,27 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
-import './global.css'
+import { Inter } from "next/font/google";
+
+import "./global.css";
+
+const inter = Inter({
+  display: "swap",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Vercel → Next.js',
-  description: 'Welcome to Next.js',
+  title: "Vercel → Next.js",
+  description: "Welcome to Next.js",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default async function Page() {
+export default function Page() {
   return (
     <div>
       <div className="author">By {process.env.NEXT_PUBLIC_AUTHOR}</div>
