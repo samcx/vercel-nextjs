c269389368bb7384b471e3a567c8747e2e5c2561
samcx <sam@vercel.com>
2024-06-14 15:03:14 -0400

    Test force-static

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-export const experimental_ppr = true
+import { randomUUID } from 'node:crypto'
+
+export const revalidate = 10
+
+export const dynamic = 'force-static'
 
 export default function Page() {
   return (
     <div>
       <div className="author">By {process.env.NEXT_PUBLIC_AUTHOR}</div>
+      <div>UUID: {randomUUID()}</div>
     </div>
   )
 }
