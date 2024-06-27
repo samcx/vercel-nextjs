c9e3fc7217b36326e1f4c8ebc89a4f43db54d358
samcx <sam@vercel.com>
2024-06-27 11:16:08 +0200

    Bump eslint-config-next

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server'
 import { randomUUID } from 'node:crypto'
+import { NextResponse } from 'next/server'
 
 export const revalidate = 10
 
--- app/bruh/[slug]/page.tsx
+++ app/bruh/[slug]/page.tsx
@@ -3,9 +3,12 @@ import { randomUUID } from 'node:crypto'
 export const revalidate = 10
 
 export default async function Page() {
-  const data = await fetch('https://next-data-api-endpoint.vercel.app/api/random', {
-    next: { tags: ['thank u, next'] },
-  }).then((res) => res.text())
+  const data = await fetch(
+    'https://next-data-api-endpoint.vercel.app/api/random',
+    {
+      next: { tags: ['thank u, next'] },
+    },
+  ).then((res) => res.text())
 
   return (
     <div>
