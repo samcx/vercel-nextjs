fc9f70c3fc75c840c59bd3a440bf5fbdadbdedf6
samcx <sam@vercel.com>
2024-08-31 22:08:47 -0700

    Fix /api/hello

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
   return Response.json({
     value: `${process.env.VERCEL}`,
-    'type of': `${typeof process.env.NODE_ENV}`,
+    'type of': `${typeof process.env.VERCEL}`,
   })
 }
