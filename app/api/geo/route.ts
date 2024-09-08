5684f0dfe2d5b7c1be56c50569294956f0751130
samcx <sam@vercel.com>
2024-09-08 11:43:45 -0700

    Add /api/geo route

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,8 +1,8 @@
-import { geolocation } from '@vercel/functions';
- 
+import { geolocation } from '@vercel/functions'
+
 export function GET(request: Request) {
-  const { city } = geolocation(request);
+  const { city } = geolocation(request)
   return new Response(`<h1>Your location is ${city}</h1>`, {
     headers: { 'content-type': 'text/html' },
-  });
+  })
 }
