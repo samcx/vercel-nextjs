ec42d9074ba38436cb43b710773f018e71a5a082
samcx <sam@vercel.com>
2024-10-07 15:45:27 -0700

    Update /api/rest, add @vercel/kv

--- app/api/rest/route.ts
+++ app/api/rest/route.ts
@@ -1,27 +1,8 @@
 import { NextResponse } from 'next/server'
-import { Octokit } from '@octokit/rest'
-// import { openai } from '@ai-sdk/openai'
-// import { generateText } from 'ai'
+import { kv } from '@vercel/kv';
 
 export async function GET() {
-  if (!process.env.GITHUB_TOKEN) throw new TypeError('GITHUB_TOKEN not set')
+  const user = await kv.hgetall('user:me');
 
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-  })
-
-  // const model = 'gpt-4o'
-
-  const { data: issues } = await octokit.rest.issues.listForRepo({
-    owner: 'vercel',
-    repo: 'next.js',
-    per_page: 25,
-  })
-
-  // const result = await generateText({
-  //   model: openai(model),
-  //   prompt: `${JSON.stringify(issues)}\nWrite a succint summary of the above issues.`,
-  // })
-
-  return NextResponse.json({ issues: `${JSON.stringify(issues)}` })
+  return NextResponse.json({ user })
 }
