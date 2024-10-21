d1036e8e1019b092d691817bc35f3f94684d748f
samcx <sam@vercel.com>
2024-10-20 22:27:54 -0700

    Update /api/rest

--- app/api/rest/route.ts
+++ app/api/rest/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { kv } from '@vercel/kv';
 
 export async function GET() {
-  const user = await kv.hgetall('user:me');
+  const scores = await kv.zrange('authorKudos', 0, -1, { withScores: true, rev: true });
 
-  return NextResponse.json({ user })
+  return NextResponse.json({ scores })
 }
