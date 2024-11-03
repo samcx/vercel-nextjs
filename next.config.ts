ab0c93668fa35c6727cf6222f0db2027b3cc5cb9
samcx <sam@vercel.com>
2024-11-03 14:23:34 -0800

    Update experimental ppr flag

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import { type NextConfig } from 'next'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
-   ppr: true,
+    ppr: true,
   },
   logging: {
     fetches: {
