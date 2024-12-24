66d3e6ff4d1bd90b0552daaa201cce62a66dcede
samcx <sam@vercel.com>
2024-12-24 13:48:24 -0800

    Update Layout prop types

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ const inter = Inter({
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en" className={inter.className}>
       <body>
