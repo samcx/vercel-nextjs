9c204603f7bc9f53ff46c393de5dfe639028a844
samcx <sam@vercel.com>
2025-10-31 11:44:42 +0900

    Bump packages

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { get, has } from '@vercel/edge-config'
 import { type NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.next()
   }
